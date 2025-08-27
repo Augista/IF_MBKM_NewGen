94b9b61cec0f067d89a226eb93c6004c39e4d3a4
Augista <augistapradana11@gmail.com>
2025-08-27 16:30:57 +0700

    feat: add error fixing

--- src/app/mbkm/grading/[id]/page.tsx
+++ src/app/mbkm/grading/[id]/page.tsx
@@ -1,21 +1,21 @@
-import Typography from "@/components/Typography";
-import { StudentDataCardContainer } from "../../components/container/StudentDataCardContainer";
-import FormGradingCard from "../../components/form/FormGradingCard";
+// import Typography from "@/components/Typography";
+// import { StudentDataCardContainer } from "../../components/container/StudentDataCardContainer";
+// import FormGradingCard from "../../components/form/FormGradingCard";
 
-export default function MbkmPage() {
+// export default function MbkmPage() {
 
-    return (
-        <div className="flex flex-col gap-8 p-8 min-h-screen">
-            <Typography
-                variant="h5"
-                weight="semibold"
-                className="text-3xl text-foreground"
-            >
-                Form Penilaian
-            </Typography>
-            <StudentDataCardContainer />
+//     return (
+//         <div className="flex flex-col gap-8 p-8 min-h-screen">
+//             <Typography
+//                 variant="h5"
+//                 weight="semibold"
+//                 className="text-3xl text-foreground"
+//             >
+//                 Form Penilaian
+//             </Typography>
+//             <StudentDataCardContainer />
 
-            <FormGradingCard />
-        </div >
-    )
-}
+//             <FormGradingCard />
+//         </div >
+//     )
+// }
